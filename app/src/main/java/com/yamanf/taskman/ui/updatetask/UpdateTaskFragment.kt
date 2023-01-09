99c429a923f59a8c7606a1b846d17708a598665d
Furkan Yaman <111470818+yamanf@users.noreply.github.com>
2023-01-10 00:01:06 +0300

    Update task screen viewmodel created.

--- app/src/main/java/com/yamanf/taskman/ui/updatetask/UpdateTaskFragment.kt
+++ app/src/main/java/com/yamanf/taskman/ui/updatetask/UpdateTaskFragment.kt
@@ -5,14 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.yamanf.taskman.R
+import com.yamanf.taskman.databinding.FragmentUpdateTaskBinding
+import com.yamanf.taskman.firebase.FirebaseRepositoryImpl
 
 class UpdateTaskFragment : Fragment() {
+    private var _binding : FragmentUpdateTaskBinding?= null
+    private val binding get() =  _binding!!
+    private val updateTaskViewModel : UpdateTaskViewModel by viewModels() {
+        UpdateTaskViewModelFactory(
+            FirebaseRepositoryImpl()
+        )
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+        _binding = FragmentUpdateTaskBinding.inflate(inflater, container,false)
 
-        return inflater.inflate(R.layout.fragment_update_task, container, false)
+
+
+
+        return binding.root
     }
 
 }
---
+++ app/src/main/java/com/yamanf/taskman/ui/updatetask/UpdateTaskViewModel.kt
@@ -0,0 +1,7 @@
+package com.yamanf.taskman.ui.updatetask
+
+import androidx.lifecycle.ViewModel
+import com.yamanf.taskman.firebase.FirebaseRepository
+
+class UpdateTaskViewModel(private val firebaseRepository: FirebaseRepository) : ViewModel() {
+}

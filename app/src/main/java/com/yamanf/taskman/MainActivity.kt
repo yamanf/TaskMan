ab759f5603a6286cf576ac4786ae29e7d80a2d78
Furkan Yaman <111470818+yamanf@users.noreply.github.com>
2024-03-02 00:06:38 +0300

    Splash API added

--- app/src/main/java/com/yamanf/taskman/MainActivity.kt
+++ app/src/main/java/com/yamanf/taskman/MainActivity.kt
@@ -1,16 +1,49 @@
 package com.yamanf.taskman
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import com.google.android.gms.ads.MobileAds
-import com.yamanf.taskman.utils.Utils
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.yamanf.taskman.firebase.FirebaseRepositoryImpl
+import com.yamanf.taskman.ui.auth.AuthActivity
+import com.yamanf.taskman.ui.onboarding.OnboardingActivity
+import com.yamanf.taskman.ui.splash.SplashScreenViewModel
+import com.yamanf.taskman.ui.splash.SplashScreenViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+    private val splashScreenViewModel: SplashScreenViewModel by viewModels() {
+        SplashScreenViewModelFactory(
+            FirebaseRepositoryImpl()
+        )
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        installSplashScreen().apply {
+            checkUser()
+        }
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        setContentView(R.layout.activity_main)
+    }
 
+    private fun checkUser(){
+        val onBoarding: SharedPreferences = getSharedPreferences("onBoardingScreen", MODE_PRIVATE)
+        val isFirstTime = onBoarding.getBoolean("firstTime",true)
+        if (splashScreenViewModel.isUserLoggedIn() && !isFirstTime) {
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        } else if (!splashScreenViewModel.isUserLoggedIn() && !isFirstTime) {
+            startActivity(Intent(this, AuthActivity::class.java))
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        } else if (isFirstTime) {
+            val editor = onBoarding.edit()
+            editor.putBoolean("firstTime", false)
+            editor.apply()
+            startActivity(Intent(this, OnboardingActivity::class.java))
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
     }
 }
